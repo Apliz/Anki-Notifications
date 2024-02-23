f93507752c375d7e174184b038dfe2931921a598
Apliz <antonplisnier@gmail.com>
2024-02-23 18:14:04 +0000

    temp disable main until infinite loop is fixed

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from helpers import network_listener
 
 def main() -> None:
     """Control flow method"""
-    deck_names, card_count = get_learnable_cards() 
-    while not network_listener():
-        message = grammar(deck_names, card_count)
-        pushover_post(message)
+    # deck_names, card_count = get_learnable_cards() 
+    # while not network_listener():
+    #     message = grammar(deck_names, card_count)
+    #     pushover_post(message)
+
+    # Currently the script turns into an infinite loop sending notifications every second.
+    # Keep main() disabled until problem is fixed
 
 if __name__ == "__main__":
     exit(main())
+``
